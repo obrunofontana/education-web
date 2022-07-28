8f346c2972c574bb0a554dc21c8cb50a1484962f
obrunofontana <obrunofontana@gmail.com>
2022-07-28 20:23:59 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './pages/course/course.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RoomComponent } from './components/room/room.component';
+import { LessonComponent } from './components/lesson/lesson.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent,
+    HeaderComponent,
+    LogoComponent,
+    SidebarComponent,
+    RoomComponent,
+    LessonComponent
   ],
   imports: [
     BrowserModule,
