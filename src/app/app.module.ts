7a184a4a0154a8c9bd5719e21ef92d8186fc55b9
obrunofontana <obrunofontana@gmail.com>
2022-08-02 21:22:11 -0300

    feat: 02-08: roteamento de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RoomComponent } from './components/room/room.component';
 import { LessonComponent } from './components/lesson/lesson.component';
 
 import { VimeModule } from '@vime/angular';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { VimeModule } from '@vime/angular';
     LogoComponent,
     SidebarComponent,
     RoomComponent,
-    LessonComponent
+    LessonComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
